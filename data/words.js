2a85add7479ca8d6bc4c4556d20179a32de63fe0
Alice Scott <alice_cy_scott@hotmail.com>
2021-01-10 12:22:59 +0000

    Added scoring and levels

---
+++ data/words.js
@@ -0,0 +1,44 @@
+const words = [
+    {
+    id: 1,
+    word: "Champion"
+    },
+    {
+    id: 2,
+    word: "Elephant"
+    },
+    {
+    id: 3,
+    word: "Fitness"
+    },
+    {
+    id: 4,
+    word: "Wardrobe"
+    },
+    {
+    id: 5,
+    word: "Professor"
+    },
+    {
+    id: 6,
+    word: "Mosquito"
+    },
+    {
+    id: 7,
+    word: "Morning"
+    },
+    {
+    id: 8,
+    word: "Basketball"
+    },
+    {
+    id: 9,
+    word: "Jungle"
+    },
+    {
+    id: 10,
+    word: "Laboratory"
+    }
+]
+
+export default words;
--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-import easyWords from "./data/easyWords.js"
+import words from "./data/words.js"
 
 const guesswordHTML = document.getElementById("guessword");
-const button = document.getElementById("new-word-button");
-const letterLines = document.getElementById("letterLines");
+const newWordButton = document.getElementById("new-word-button");
 const inputLetter = document.querySelector("#inputLetter");
 const inputButton = document.querySelector("#inputButton");
-const incorrectLettersHTML = document.getElementById("guessedLetters")
+const incorrectLettersHTML = document.getElementById("guessedLetters");
+const hangman = document.getElementById("hangman");
+const scoreDisplay = document.getElementById("score-display");
+const levelDisplay = document.getElementById("level");
+const loserCard = document.getElementById("loser-card")
+const newGame = document.getElementById("new-game-button");
 
+let livesLost = 0;
+let score = 0;
+let level = "Noob";
+
+let findWord;
+let incorrectLetters = [];
+let correctLetters = [];
+let guessedLetter;
 let splitWord;
+let lettersLeft;
+let spanArray;
+let letterArray;
+
+// Add losing text for each level in new level.js file
+// Add more words to words.js file
 
 const showWord = () => {
     guesswordHTML.innerHTML = ""
@@ -18,34 +36,121 @@ const showWord = () => {
 
 const selectWord = () => {
     let randomNumber = Math.floor((Math.random() * 10) + 1)
-    const findWord = (easyWords.filter(word => word.id === randomNumber))[0]; 
+    findWord = (words.filter(word => word.id === randomNumber))[0]; 
     splitWord = findWord.word.split("");
+    lettersLeft = findWord.word.length;
     showWord();
 };
 
-const showLetterLines = () => {
-    let numberOfLetters = splitWord.length;
-    letterLines.innerText = ("_ ".repeat(numberOfLetters));
+const showHangman = () => {
+    hangman.src = `./assets/images/${livesLost}.png`;
 }
 
-button.addEventListener("click", () => {
-    selectWord()
-    showLetterLines()
-    });
-
-inputButton.addEventListener("click", () => {
-    let guessword = (guesswordHTML.innerText);
-    const guessedLetter = inputLetter.value.toLowerCase();
-    const isLetterMatch = (guessword.toLowerCase().split("")).find(letter => letter === guessedLetter);
-    if(isLetterMatch) {
-        const spanArray = Array.from(guesswordHTML.children);
-        const x = spanArray.map(span => span.innerText.toLowerCase());
-        const letterPosition = x.indexOf(isLetterMatch);
-        const letterHtml = spanArray[letterPosition];
-        letterHtml.style.color = "black";
+const checkLetter = () => {
+    if(letterArray.includes(guessedLetter)) {
+        spanArray.forEach(span => {
+            const letter = (span.innerText).toLowerCase();
+            if(letter === guessedLetter) {
+                span.style.color = "black";
+                correctLetters.push(guessedLetter);
+                lettersLeft--;
+            } 
+        })
+    } else if(!guessedLetter) {
+        alert("Please enter a letter");
     } else {
+        livesLost++;
         incorrectLettersHTML.innerText += guessedLetter + "";
+        incorrectLetters.push(guessedLetter);
     }
     inputLetter.value = "";
+    showHangman();
+    isAlive();
+}
+
+const showScore = () => {
+    scoreDisplay.innerText = `Score: ${score}`
+}
+
+const showLevel = () => {
+    if(score > 2) {
+        level = "Rookie"
+    } else if(score > 5) {
+        level = "Skilled"
+    } else if(score > 8) {
+        level = "Expert"
+    } else if(score > 11) {
+        level = "Grand Master"
+    } else if(score > 14) {
+        level = "Legend"
+    }
+    levelDisplay.innerText = `Level: ${level}`
+}
+
+const win = () => {
+    score++;
+    resetGame();
+}
+
+const lose = () => {
+    loserCard.style.visibility = "visible";
+    loserCard.innerHTML = `
+    <h2>You Lose!</h2>
+    <h3>Score: ${score}</h3>
+    <h3>Level: ${level}</h3>
+    <p>You're a NOOB! But it's okay because practice makes perfect. (And it's only a game.)</p>
+    `
+}
+
+const isAlive = () => {
+    if(livesLost === 12)
+    lose();
+}
+
+const handleInput = (e) => {
+    spanArray = Array.from(guesswordHTML.children);
+    letterArray = spanArray.map(span => span.innerText.toLowerCase());
+    guessedLetter = inputLetter.value.toLowerCase();
+        if(incorrectLetters.includes(guessedLetter) || correctLetters.includes(guessedLetter) ) {
+            alert("This letter has already been guessed");
+        } else {
+            checkLetter();
+        }
+    if(lettersLeft === 0) {
+        win();
+    }
+    showScore();
+    showLevel();
+}
+
+const resetGame = () => {
+    lettersLeft = 12
+    correctLetters = [];
+    incorrectLetters = [];
+    livesLost = 0;
+    incorrectLettersHTML.innerText = "";
+    selectWord()
+    showHangman();
+}
+
+newWordButton.addEventListener("click", () => {
+    resetGame();
+    });
+
+inputButton.addEventListener("click", (e) => {
+    handleInput(e);
 })
 
+inputLetter.addEventListener("keyup", (e) => {
+    if(e.key === "Enter") {
+        handleInput(e);
+    }
+})
+
+selectWord();
+
+newGame.addEventListener("click", () => {
+    resetGame();
+    loserCard.style.visibility = "hidden";
+    score = 0;
+})
